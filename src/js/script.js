2e63e237b5cba400b73e5546ccd57761e34d4d0d
Aleksey <example@gmail.com>
2021-06-21 22:20:22 +0700

    add slider

---
+++ src/js/script.js
@@ -0,0 +1,9 @@
+
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        adaptiveHeight: true,
+        speed: 1200,
+        prevArrow: '<button type="button" class="slick-prev"><img src="../icons/prev.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="../icons/next.svg"></button>'
+    });
+  });
