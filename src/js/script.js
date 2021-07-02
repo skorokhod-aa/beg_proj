35feb8bc51fda7161791007a33f72760d37283a8
Aleksey <example@gmail.com>
2021-07-02 22:49:12 +0700

    last commit

--- src/js/script.js
+++ src/js/script.js
@@ -29,5 +29,43 @@ $(document).ready(function(){
         })
     });
 
+    //modal
+
+    $('[data-modal=consultation]').on('click',function(){
+        $('.overlay, #consultation').fadeIn('slow');
+    });
+    $('.modal__close').on('click', function(){
+        $('.overlay, #consultation, #order, #thanks').fadeOut('slow');
+    });
+    $('.button_mini').on('click',function(){
+        $('.overlay, #order').fadeIn('slow');
+    });
+
+    $('#consultation-form').validate();
+    $('#consultation form').validate();
+    $('#order form').validate();
+
+    $('form').submit(function(e){
+        e.preventDefault();
+        $.ajax({
+            type: "POST",
+            url: "mailer/smart.php",
+            data: $(this).serialize()
+        }).done(function(){
+            $(this).find("input").val("");
+
+            $("form").trigger('reset');
+        })
+        return false;
+    });
+
+    $(window).scroll(function(){
+        if ($(this).scrollTop() > 1600){
+            $('.pageup').fadeIn();
+        } else{
+            $('.pageup').fadeOut();
+        }
+    })
+
   });
  
